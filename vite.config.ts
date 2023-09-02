d20dda71f9f50ebf65dccb39bc028d70495430e2
n1md7 <b.kodua@gmail.com>
2023-09-02 23:31:29 +0300

    chore: tmp fix path

--- vite.config.ts
+++ vite.config.ts
@@ -4,42 +4,44 @@ import { defineConfig } from 'vite';
 import glsl from 'vite-plugin-glsl';
 import solidPlugin from 'vite-plugin-solid';
 
-export default defineConfig({
-  publicDir: 'public',
-  envPrefix: 'VITE_',
-  envDir: process.cwd(),
-  server: {
-    port: 4096,
-    host: '0.0.0.0',
-    open: '#debug',
-  },
-  resolve: {
-    alias: {
-      '@': process.cwd(),
+export default defineConfig((env) => {
+  return {
+    publicDir: 'public',
+    envPrefix: 'VITE_',
+    envDir: process.cwd(),
+    server: {
+      port: 4096,
+      host: '0.0.0.0',
+      open: '#debug',
     },
-  },
-  base: './',
-  build: {
-    chunkSizeWarningLimit: 700,
-    sourcemap: true,
-    assetsDir: '.',
-    emptyOutDir: true,
-    rollupOptions: {
-      input: {
-        index: 'index.html',
+    resolve: {
+      alias: {
+        '@': process.cwd(),
       },
     },
-  },
-  plugins: [glsl(), solidPlugin()],
-  test: {
-    setupFiles: ['./tests/unit/__setup__/setup.ts'],
-    globals: true,
-    environment: 'jsdom',
-    coverage: {
-      all: true,
-      provider: 'v8',
-      reporter: ['cobertura', 'text', 'html'],
-      exclude: ['*.cjs', '*.config.*', 'dist/**', 'src/**.d.ts', 'tests'],
+    base: env.mode === 'production' ? '/mini-slot/' : './',
+    build: {
+      chunkSizeWarningLimit: 700,
+      sourcemap: true,
+      assetsDir: '.',
+      emptyOutDir: true,
+      rollupOptions: {
+        input: {
+          index: 'index.html',
+        },
+      },
     },
-  },
+    plugins: [glsl(), solidPlugin()],
+    test: {
+      setupFiles: ['./tests/unit/__setup__/setup.ts'],
+      globals: true,
+      environment: 'jsdom',
+      coverage: {
+        all: true,
+        provider: 'v8',
+        reporter: ['cobertura', 'text', 'html'],
+        exclude: ['*.cjs', '*.config.*', 'dist/**', 'src/**.d.ts', 'tests'],
+      },
+    },
+  };
 });

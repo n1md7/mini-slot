36dc0c8ae13bf5c57c6b441dcf2e6d13006d5419
n1md7 <b.kodua@gmail.com>
2023-09-02 18:46:45 +0300

    chore: coloring improvements

--- src/game/components/reels/components/Point.ts
+++ src/game/components/reels/components/Point.ts
@@ -2,14 +2,15 @@ import { Graphics } from 'pixi.js';
 
 export class Point extends Graphics {
   static width = 8;
+  static height = 4;
   constructor(x: number, y: number) {
     super();
 
-    this.y = y;
+    this.y = y - Point.height / 2;
     this.x = x;
 
-    this.beginFill('#6A6E6AE8');
-    this.drawRect(0, 0, Point.width, 1);
+    this.beginFill('#7691dc', 0.8);
+    this.drawRoundedRect(0, 0, Point.width, Point.height, 4);
     this.endFill();
     this.zIndex = 2;
   }
